af9586bfe5d9433c784169e4f2630d7cc7f6d1c7
Aslam-16 <54734988+Aslam-16@users.noreply.github.com>
2020-12-29 18:37:16 +0530

    Update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -65,7 +65,7 @@ class Login extends React.Component{
         email:mail,
         password:password
       }
-      axios.post("http://localhost:3001/api/v1/users/isAuth",formdata)
+      axios.post("https://bdd98d5588f4.ngrok.io/api/v1/users/isAuth",formdata)
       .then((response) => {
         if(response.data.status){
           localStorage.setItem("id",response.data.data._id)
